1f98b78420c4d21c0bceb0bed59bb0ddf3953207
koy <kosatun@aol.com>
2019-05-10 00:42:13 -0700

    created carousel, pie chart, and modified css

--- js/script.js
+++ js/script.js
@@ -17,4 +17,43 @@ $(document).ready(function() {
     startDelay: 1000,
     showCursor: false
   });
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      939: {
+        items: 4
+      }
+    }
+  });
+
+  var skillsTopOffset = $(".skills-section").offset().top;
+
+  $(window).scroll(() => {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#e600e6",
+        trackColor: "#f2f2f2",
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+          $(this.el)
+            .find(".percent")
+            .text(Math.round(percent));
+        }
+      });
+    }
+  });
 });
